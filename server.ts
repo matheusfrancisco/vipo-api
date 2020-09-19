c13a122a9ed89ce74711fd42ab890c4482e9a2be
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-09-19 16:51:18 -0300

    Add server initial

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express, { Response } from 'express';
+
+export const factoryServer = ({ userController }: any) => {
+  const server = express();
+
+  server.use(express.json());
+
+  server.get('/', (_, res: Response) => {
+    res.send('Hello world');
+  });
+
+ 
+  return server;
+};
