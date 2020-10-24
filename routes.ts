1f9cd5cfeaf86006e30c001a73da35d855eb97fc
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-10-24 17:04:10 -0300

    Wrapper server to inject routes

--- routes.ts
+++ routes.ts
@@ -1,12 +1,33 @@
 import { Router } from "express";
 import { createUseCaseFactory } from "./src/useCases/CreateUser";
+import { FindUseCaseFactory } from "./src/useCases/FindUser";
+import { Auth } from "./src/middlewares/auth";
+import { CreateDatabaseConnection } from "./src/infrastructure/connection";
 
-export const routerFactory = async () => {
-  const { userController } = await createUseCaseFactory.build();
+export const routerFactory = async (config: string = "prod") => {
+  const connection = await CreateDatabaseConnection.createConnection();
+
+  const {
+    createUserController,
+    userRepository
+  } = await createUseCaseFactory.build(connection);
+
+  // const { findUserController } = await FindUseCaseFactory.build(connection);
+
+  const auth = await new Auth(userRepository);
   const router = Router();
 
+  router.get("/signin", auth.singIn, (request, response) => {
+    // const userProfileInformation = findUserController.handle(request, response);
+    // #TODO remove sensitive informations from user
+    return response.status(200).send({
+      user: request.body.user,
+      token: request.body.token
+    });
+  });
+
   router.post("/users", (request, response) => {
-    return userController.handle(request, response);
+    return createUserController.handle(request, response);
   });
 
   return router;
