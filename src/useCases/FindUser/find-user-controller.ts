25b32f7efe3eecdf579a201249fb5106560109b3
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-10-24 17:05:53 -0300

    Add findUseCase

---
+++ src/useCases/FindUser/find-user-controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { FindUserUseCase } from "./find-user-use-case";
+import { UserEntity } from "src/infrastructure/entity/user-entity";
+const buildErrorMessage = (message: string) => ({ error: message });
+
+export class FindUserController {
+  private _findUserUseCase: FindUserUseCase;
+
+  constructor(findUserUseCase: FindUserUseCase) {
+    this._findUserUseCase = findUserUseCase;
+  }
+
+  async handle(
+    request: Request,
+    response: Response
+  ): Promise<UserEntity | undefined | Response> {
+    if (!request.body || !request.body.name || !request.body.email) {
+      response.status(400).json(buildErrorMessage("Parameters missing"));
+      return;
+    }
+    try {
+      const user = await this._findUserUseCase.execute({
+        name: request.body.name,
+        email: request.body.email
+      });
+
+      return user;
+    } catch (err) {
+      // const status = error.constructor.name === "ServiceError" ? 400 : 500;
+      // res.status(status).json(buildErrorMessage(error.message));
+      return response.status(400).json({
+        message: err.message || "Unexpected error."
+      });
+    }
+  }
+}
---
+++ src/useCases/FindUser/index.ts
@@ -0,0 +1,20 @@
+import { PostgresCustomerRepository } from "../../infrastructure/postgres-customer-repository";
+import { FindUserController } from "./find-user-controller";
+import { Connection, createConnection } from "typeorm";
+import { CustomerRepository } from "../../domain/user/user-repository";
+import { FindUserUseCase } from "./find-user-use-case";
+
+export class FindUseCaseFactory {
+  public static async build(connection: Connection) {
+    let userRepository: CustomerRepository;
+
+    userRepository = new PostgresCustomerRepository(connection);
+
+    const findUseCases = new FindUserUseCase(userRepository);
+
+    const findUserController = new FindUserController(findUseCases);
+    return {
+      findUserController
+    };
+  }
+}
