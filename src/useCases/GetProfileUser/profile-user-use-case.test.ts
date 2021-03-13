f0eded36231891bfe205c94bf2760624f3703015
Raul <raul.hmetal@gmail.com>
2021-03-13 10:52:48 -0300

    Edit: Returning the users data

--- src/useCases/GetProfileUser/profile-user-use-case.test.ts
+++ src/useCases/GetProfileUser/profile-user-use-case.test.ts
@@ -1,7 +1,61 @@
+import MockUserRepository from "@domain/user/mocks/mock-user-repository";
+import { ProfileUserUseCase } from "@useCases/GetProfileUser/profile-user-use-case";
+
 describe("Profile User Use Case", () => {
-  it.todo("should return undefined when no user exists");
+  it("should return undefined when no user exists", async () => {
+    const usersRepository = new MockUserRepository();
+    const useCase = new ProfileUserUseCase(usersRepository);
 
-  it.todo("should return undefined when no user profile exists");
+    const email = "my_random_mail@email.com";
 
-  it.todo("should return the user and its profile correctly");
+    const result = await useCase.execute({ email });
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should return undefined when no user profile exists", async () => {
+    const usersRepository = new MockUserRepository();
+    usersRepository.findByEmail = jest.fn(() => ({
+      id: 20,
+      name: "Fake"
+    }));
+
+    const useCase = new ProfileUserUseCase(usersRepository);
+
+    const email = "my_random_mail@email.com";
+
+    const result = await useCase.execute({ email });
+
+    expect(result).toBeUndefined();
+    expect(usersRepository.findUserProfile).toHaveBeenCalled();
+  });
+
+  it("should return the user and its profile correctly", async () => {
+    const user = {
+      name: "Fake",
+      lastName: "Fakerson",
+      email: "my_random_mail@email.com",
+      birthDate: "15/11/19900",
+      gender: "male"
+    };
+
+    const profile = {
+      drinks: [],
+      foods: [],
+      musicals: ["something"]
+    };
+
+    const usersRepository = new MockUserRepository();
+    usersRepository.findByEmail = jest.fn(() => user);
+    usersRepository.findUserProfile = jest.fn(() => profile);
+
+    const useCase = new ProfileUserUseCase(usersRepository);
+
+    const result = await useCase.execute({ email: user.email });
+
+    expect(result).toEqual({
+      ...user,
+      profileInformations: profile
+    });
+  });
 });
