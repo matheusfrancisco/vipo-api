4a322c64555cc850f27b20448717832efd2a9381
matheusfrancisco <matheusmachadoufsc@gmail.com>
2021-07-10 20:21:57 -0300

    fix run serverless local

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,36 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
+/*
+This line is only required if you are specifying `TS_NODE_PROJECT` for whatever reason.
+ */
+// delete process.env.TS_NODE_PROJECT;
+
 module.exports = {
-
-
+  context: __dirname,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: slsw.lib.webpack.isLocal
+    ? 'eval-cheap-module-source-map'
+    : 'source-map',
   resolve: {
     extensions: ['.mjs', '.json', '.ts'],
     symlinks: false,
     cacheWithContext: false,
     plugins: [
       new TsconfigPathsPlugin({
-        configFile: './tsconfig.json',
+        configFile: './tsconfig.paths.json',
       }),
     ],
   },
-
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
   optimization: {
     concatenateModules: false,
   },
-
   target: 'node',
   externals: [nodeExternals()],
   module: {
