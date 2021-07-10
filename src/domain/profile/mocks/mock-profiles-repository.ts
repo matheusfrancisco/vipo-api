409e574d329d3a7b31c89f68077209d109ef9889
Raul <raul.hmetal@gmail.com>
2021-07-10 19:43:48 -0300

    Test: Added mock profiles repository

--- src/domain/profile/mocks/mock-profiles-repository.ts
+++ src/domain/profile/mocks/mock-profiles-repository.ts
@@ -1,7 +1,48 @@
+import { IEntityId } from "@domain/global";
+import IProfile from "@domain/profile/IProfile";
 import IProfilesRepository from "@domain/profile/IProfilesRepository";
+import Profile from "@domain/profile/profile";
+import MockUserData from "@domain/user/mocks/mock-user-data";
 
 export default class MockProfilesRepository implements IProfilesRepository {
-  public findByUser = jest.fn();
+  private profiles: IProfile[] = [];
 
-  public createOrUpdateOne = jest.fn();
+  public async findByUser(id: IEntityId): Promise<IProfile | undefined> {
+    return this.profiles.find(profile => profile.user.id === id);
+  }
+
+  public async save({
+    id,
+    user,
+    musicals,
+    drinks,
+    foods
+  }: Profile): Promise<IProfile> {
+    const mockedUser = new MockUserData();
+
+    const newProfile: IProfile = {
+      id: id || this.profiles.length + 1,
+      user: {
+        ...mockedUser,
+        id: user
+      },
+      musicals,
+      drinks,
+      foods
+    };
+
+    const foundIndex = this.profiles.findIndex(profile => profile.id === id);
+
+    if (!foundIndex) {
+      this.profiles.push(newProfile);
+
+      return newProfile;
+    }
+
+    const profile = this.profiles[foundIndex];
+    Object.assign(profile, { musicals, drinks, foods, user });
+    this.profiles[foundIndex] = profile;
+
+    return profile;
+  }
 }
