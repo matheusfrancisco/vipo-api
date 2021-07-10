0a8eaae487454a5a4290fdc06653b6294347efc8
Raul <raul.hmetal@gmail.com>
2021-07-10 19:12:12 -0300

    Test: mocking user repository

---
+++ src/domain/user/IUserRepository.ts
@@ -0,0 +1,16 @@
+import { IUserData } from "@domain/user/IUser";
+import UserData from "@domain/user/user-data";
+
+export interface IUserRepositoryUpdatePayload {
+  userId: number;
+  name?: string;
+  lastName?: string;
+  password?: string;
+}
+
+export default interface IUserRepository {
+  save: (user: UserData) => Promise<IUserData>;
+  findByEmail: (email: string) => Promise<IUserData | undefined>;
+  update: (user: IUserRepositoryUpdatePayload) => Promise<IUserData>;
+  updateResetPasswordToken: (id: number, token?: string) => Promise<void>;
+}
--- src/domain/user/mocks/mock-user-repository.ts
+++ src/domain/user/mocks/mock-user-repository.ts
@@ -1,12 +1,70 @@
-import IUser from "@domain/user/IUser";
-import { IUserRepository } from "@domain/user/user-repository";
+import { IUserData } from "@domain/user/IUser";
+import IUserRepository, {
+  IUserRepositoryUpdatePayload
+} from "@domain/user/IUserRepository";
+import UserData from "@domain/user/user-data";
 
 export default class MockUserRepository implements IUserRepository {
-  public save = jest.fn();
+  private users: IUserData[] = [];
 
-  public findByEmail = jest.fn();
+  public async save({
+    name,
+    lastName,
+    gender,
+    email,
+    password,
+    birthDate,
+    resetPasswordToken
+  }: UserData): Promise<IUserData> {
+    const newUser: IUserData = {
+      id: this.users.length + 1,
+      name,
+      lastName,
+      gender,
+      email,
+      birthDate,
+      resetPasswordToken,
+      password
+    };
 
-  public update = jest.fn(() => Promise.resolve({} as IUser));
+    this.users.push(newUser);
 
-  public updateResetPasswordToken = jest.fn();
+    return newUser;
+  }
+
+  public async findByEmail(email: string): Promise<IUserData | undefined> {
+    return this.users.find(user => user.email === email);
+  }
+
+  public async update({
+    userId,
+    name,
+    lastName,
+    password
+  }: IUserRepositoryUpdatePayload): Promise<IUserData> {
+    const userIndex = this.users.findIndex(user => user.id === userId);
+    if (!userIndex) throw new Error("User does not exist");
+
+    const user = this.users[userIndex];
+    if (name) user.name = name;
+    if (lastName) user.lastName = lastName;
+    if (password) user.password = password;
+
+    this.users[userIndex] = user;
+
+    return user;
+  }
+
+  public async updateResetPasswordToken(
+    id: number,
+    token?: string
+  ): Promise<void> {
+    const userIndex = this.users.findIndex(user => user.id === id);
+    if (!userIndex) throw new Error("User does not exist");
+
+    const user = this.users[userIndex];
+    user.resetPasswordToken = token;
+
+    this.users[userIndex] = user;
+  }
 }
